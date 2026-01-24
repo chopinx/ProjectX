7b20e380e94309a7a80d11835a2a71943fb7f52b
Qinbang Xiao <qinbang.xiao@cresta.ai>
2026-01-24 09:59:26 +0100

    Initial Commit

---
+++ ProjectX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectX
+//
+//  Created by Qinbang Xiao on 24.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectX/ProjectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectXApp.swift
+//  ProjectX
+//
+//  Created by Qinbang Xiao on 24.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
